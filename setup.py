6c4a00cc9cf551a00b8a71e9479bd40626db8935
Remi Rampin <remirampin@gmail.com>
2013-10-01 11:40:41 -0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+description = """
+file_archive is a file archiving system. You submit it files with a set of
+metadata, as key-value pairs, and it allows you to later retrieve the files
+that match conditions on these metadata.
+
+It uses a flat file-store where files are stored under their 40 characters SHA1
+hash, and a SQLite3 database for the metadata.
+
+Its purpose is to be used as a persistent file store for the VisTrails workflow
+and provenance management system: http://www.vistrails.org/
+"""
+setup(name='file_archive',
+      version='0.1',
+      packages=['file_archive'],
+      entry_points={
+          'console_scripts': ['file_archive = file_archive.main:main']},
+      description='A file store with searchable metadata',
+      author="NYU-Poly",
+      author_email='vistrails-dev@vistrails.org',
+      url='http://github.com/remram44/logstack',
+      long_description=description,
+      license='Modified BSD License',
+      keywords=['file', 'archive', 'metadata'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: System :: Archiving'])
