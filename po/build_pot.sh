251332ddf9b764f84ceba1127fd9c5859b8add32
Remi Rampin <remirampin@gmail.com>
2013-10-21 18:01:21 -0400

    Adds _n() to po/build_pot.sh

--- po/build_pot.sh
+++ po/build_pot.sh
@@ -5,5 +5,6 @@ DIR=$(dirname $0)/..
 exec xgettext \
     --language=Python \
     --keyword=_ \
+    --keyword=_n:1,2 \
     --output=$DIR/po/file_archive.pot \
     $(find $DIR/file_archive -name '*.py')
