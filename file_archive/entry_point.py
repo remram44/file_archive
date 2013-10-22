5a09c981858cf753c582984f6812cb85a3bd8b13
Remi Rampin <remirampin@gmail.com>
2013-10-22 13:05:00 -0400

    Use a writer from codecs for output streams

--- file_archive/entry_point.py
+++ file_archive/entry_point.py
@@ -1,3 +1,4 @@
+import codecs
 import locale
 import sys
 
@@ -6,6 +7,16 @@ from file_archive.trans import setup_translation
 
 
 def entry_point():
+    # Locale
     locale.setlocale(locale.LC_ALL, '')
+
+    # Encoding for output streams
+    if str == bytes:
+        writer = codecs.getwriter(locale.getpreferredencoding())
+        sys.stdout = writer(sys.stdout)
+        sys.stderr = writer(sys.stderr)
+
+    # Load gettext translation catalog
     setup_translation()
+
     main(sys.argv[1:])
