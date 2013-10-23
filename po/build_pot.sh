4ee38fbdc17c0a73b671d0f5efd2e53897ada258
Remi Rampin <remirampin@gmail.com>
2013-10-23 10:12:31 -0400

    Updates POT file

--- po/build_pot.sh
+++ po/build_pot.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 DIR=$(dirname $0)/..
+cd "$DIR"
 
 exec xgettext \
     --language=Python \
     --keyword=_ \
     --keyword=_n:1,2 \
-    --output=$DIR/po/file_archive.pot \
-    $(find $DIR/file_archive -name '*.py')
+    --output=po/file_archive.pot \
+    $(find file_archive -name '*.py')
